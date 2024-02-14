3dcdc35ccd6dc01fa756dcf427c06cc242189970
Vinícius Augusto <47228577+Dev-Vinicius1@users.noreply.github.com>
2024-02-14 15:22:16 -0300

    Add files via upload

---
+++ src/main/java/org/example/exec3.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
+public class exec3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.printf("Diferença = %d", (a*b) - (c*d));
+
+        sc.close();
+
+    }
+}
