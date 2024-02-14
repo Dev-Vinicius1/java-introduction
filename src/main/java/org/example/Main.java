e0527d897159feca52f9b2f41c48d37eddd57620
Vinícius <vinicius.live23@gmail.com>
2024-02-14 13:54:34 -0300

    subindo o exercícios 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int sum = x+y;
+
+        System.out.println("SOMA = " + sum);
+
+        sc.close();
+    }
+}
