942b030ce7c11ca08cee3efc9a5477dad884010b
Vinícius Augusto <47228577+Dev-Vinicius1@users.noreply.github.com>
2024-02-14 13:57:51 -0300

    Update and rename Main.java to exec1.java

--- src/main/java/org/example/exec1.java
+++ src/main/java/org/example/exec1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 mensagem explicativa, conforme exemplos.*/
 
-public class Main {
+public class exec1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
