b4f2bad8514d7b2c3f74d752e006f82f185ec219
Vinícius Augusto <47228577+Dev-Vinicius1@users.noreply.github.com>
2024-02-15 19:20:29 -0300

    Add files via upload

---
+++ src/main/java/org/example/exec21.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class exec21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int fatorial = 1;
+
+        for(int i=1;i<=n;i++){
+            fatorial = fatorial * i;
+        }
+
+        System.out.println(fatorial);
+
+        sc.close();
+
+    }
+}
